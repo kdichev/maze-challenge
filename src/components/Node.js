eccfdc47dbfc89aef23fe3e9d96d575db4be9f0a
kdichev <konstantin.dichev@gmail.com>
2018-09-09 21:34:22 +0200

    pass node props instead of whole node

--- src/components/Node.js
+++ src/components/Node.js
@@ -5,11 +5,11 @@ const Node = styled.div`
   width: 50px;
   height: 50px;
   background-color: lightgray;
-  background-color: ${props => props.node.includes("pony") && `pink`};
-  background-color: ${props => props.node.includes("exit") && `green`};
-  background-color: ${props => props.node.includes("domokun") && `red`};
-  border-left: ${props => props.node.includes("west") && `1px solid black;`};
-  border-top: ${props => props.node.includes("north") && `1px solid black;`};
+  background-color: ${props => props.isPony && `pink`};
+  background-color: ${props => props.isEndPoint && `green`};
+  background-color: ${props => props.isDomokun && `red`};
+  border-left: ${props => props.walls.includes("west") && `1px solid black;`};
+  border-top: ${props => props.walls.includes("north") && `1px solid black;`};
 `;
 
 export default Node;
