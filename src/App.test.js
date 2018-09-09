fc1f82b1b5f75ad7e0466c82e72b619ba0154690
kdichev <konstantin.dichev@gmail.com>
2018-09-09 22:10:00 +0200

    fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import configureMockStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  const store = mockStore({
+    mazeState: {
+      data: { data: [], size: [1], pony: [1], domokun: [1], endPoint: [1] }
+    }
+  });
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
